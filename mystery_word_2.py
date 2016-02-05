bf838e39d285287533adacfe89f5c898a88c7ea5
Doddecahedron <mpdodd7@gmail.com>
2016-02-05 09:41:58 -0500

    Can you guess the word?

--- mystery_word_2.py
+++ mystery_word_2.py
@@ -1,29 +1,29 @@
 import random
 
 word_file = "/usr/share/dict/words"
-WORDS = open(word_file).read().splitlines()
+WORDS = open(word_file).read().lower().split()
 secret_word = random.choice(WORDS)
+bad_guesses = []
+good_guesses = []
 
 while True:
     start = input("Press enter to start, or Q to quit")
     if start.lower() == 'q':
         break
 
-    bad_guesses = []
-    good_guesses = []
-
     while len(bad_guesses) < 8 and len(good_guesses) != len(list(secret_word)):
         for letter in secret_word:
             if letter in good_guesses:
-                print(letter, end="") # lets print print multiple times on the same line
+                print(letter, end="")
             else:
                 print('_', end="")
-        print('Strikes: {}/8'.format(len(bad_guesses)))
+        print('  Strikes: {}/8'.format(len(bad_guesses)))
 
-        guess = input("Guess a letter: ").lower()
+        guess = input("{} letters in your word. Guess a letter: ".format(len(list(secret_word)))).lower()
+        print("")
 
         if len(guess) != 1:
-            print("You can only guess a single letter.")
+            print("You can only guess a single letter. Try again.")
             continue
         elif guess in bad_guesses or guess in good_guesses:
             print("You've already guessed that letter.")
